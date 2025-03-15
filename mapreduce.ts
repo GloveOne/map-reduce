00e2a55593e198198212746f7b01557b12ab7df2
Gustavo <navarro.gu@gmail.com>
2025-03-15 17:11:07 -0300

    Add reduce, every, some and forEach

--- mapreduce.ts
+++ mapreduce.ts
@@ -8,8 +8,6 @@ function mapG<A, B>(array: Array<A>, fn: (item: A) => B): Array<B> {
   return arr;
 }
 
-mapG([1, 2, 3], (item) => item.toString());
-
 function flatMapG<A, B>(array: Array<A>, fn: (item: A) => Array<B>): Array<B> {
   //const arr: Array<B> = [];
   //for(const item of array){
@@ -21,6 +19,46 @@ function flatMapG<A, B>(array: Array<A>, fn: (item: A) => Array<B>): Array<B> {
     (result: Array<B>, item: A) => [...result, ...fn(item)],
     []
   );
+}
+
+function filterG<A>(array: Array<A>, fn: (item: A) => boolean): Array<A> {
+  const arr: Array<A> = [];
+  for (const i of array) {
+    if (fn(i)) arr.push(i);
+  }
+  return arr;
+}
+
+function reduceG<A, B>(
+  array: Array<A>,
+  initValue: B,
+  redFn: (i: B, j: A) => B
+): B {
+  let arr: B = initValue;
+  for (const i of array) {
+    arr = redFn(arr, i);
+  }
+  return arr;
+}
+
+function everyG<A>(array: Array<A>, boFn: (item: A) => boolean): boolean {
+  for (const i of array) {
+    if (!boFn(i)) return false;
+  }
+  return true;
+}
+
+function someG<A>(array: Array<A>, boFn: (item: A) => boolean): boolean {
+  for (const i of array) {
+    if (boFn(i)) return true;
+  }
+  return false;
+}
+
+function forEachG<A, B>(array: Array<A>, fn: (item: A) => B): undefined {
+  for (const i of array) {
+    fn(i);
+  }
 }
 
 Deno.test("map test", () => {
@@ -39,7 +77,54 @@ Deno.test("map on empty array", () => {
 
 Deno.test("flatmap", () => {
   assertEquals(
-    flatMapG([1, 2, 3], (n) => [n, n**2]),
+    flatMapG([1, 2, 3], (n) => [n, n ** 2]),
     [1, 1, 2, 4, 3, 9]
+  );
+});
+
+Deno.test("filter", () => {
+  assertEquals(
+    filterG([1, 2, 3, 4, 5, 6, 7, 8, 9], (n) => n % 2 === 0),
+    [2, 4, 6, 8]
+  );
+});
+
+Deno.test("Reduce", () => {
+  assertEquals(
+    reduceG([1, 2, 3, 4, 5, 6, 7, 8, 9], 0, (n, m) => n + m),
+    45
+  );
+  assertEquals(
+    reduceG([1, 2, 3, 4, 5, 6, 7, 8, 9], 1, (n, m) => n * m),
+    362880
+  );
+});
+
+Deno.test("Reduce2.0", () => {
+  assertEquals(
+    reduceG([1, 2, 3, 4, 5, 6, 7, 8, 9], 1, (n, m) => n ** m),
+    [1, 2, 3, 4, 5, 6, 7, 8, 9].reduce((n, m) => n ** m, 1)
+  );
+});
+
+Deno.test("Every", () => {
+  assertEquals(
+    everyG([1, 2, 3, 4, 5, 6, 7, 8, 9], (n) => typeof n === "number"),
+    true
+  );
+  assertEquals(
+    everyG([1, 2, 3, 4, 5, 6, 7, 8, 9], (n) => typeof n === "undefined"),
+    false
+  );
+});
+
+Deno.test("Some", () => {
+  assertEquals(
+    someG([1, 2, 3, 4, 5, 6, 7, 8, 9], (n) => typeof n === "number"),
+    true
+  );
+  assertEquals(
+    someG([1, 2, 3, 4, 5, 6, 7, 8, 9], (n) => typeof n === "undefined"),
+    false
   );
 });
